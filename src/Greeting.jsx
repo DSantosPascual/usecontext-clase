4adf787342a900f96f88713cd0b7435b1656efe0
DSantosPascual <d.santos.pascual@gmail.com>
2025-03-21 13:04:10 +0100

    clase terminada

---
+++ src/Greeting.jsx
@@ -0,0 +1,20 @@
+import { useLanguage } from './LanguageContext';
+
+const Greeting = () => {
+    const { language } = useLanguage()
+
+    const greetings = {
+        en: 'Hello!',
+        es: 'Hola!',
+        fr: 'Salut!'
+    }
+
+    return (
+        <>
+            <h2>{greetings[language]}</h2>
+        </>
+
+    )
+}
+
+export default Greeting;
